2c0f392df5c542b39b66909e6460fd096f228dc0
ChenXiao <chenxiao07@baidu.com>
2015-10-13 11:07:57 +0800

    添加Region组件

--- src/calendar/Header.js
+++ src/calendar/Header.js
@@ -1,6 +1,6 @@
 /**
  * @file Calendar/CalendarHeader
- * @author cxtom(cxtom2008@gmail.com)
+ * @author cxtom(cxtom2010@gmail.com)
  */
 
 var React = require('react');
---
+++ src/region/helper.js
@@ -0,0 +1,38 @@
+/**
+ * @file melon/region/mixin
+ * @author cxtom(cxtom2010@gmail.com)
+ */
+
+var _ = require('underscore');
+
+var helper = {
+    selectAll: function (child) {
+        child.selected = true;
+        _.isArray(child.children) && _.each(child.children, helper.selectAll);
+    },
+    cancelAll: function (child) {
+        child.selected = false;
+        _.isArray(child.children) && _.each(child.children, helper.cancelAll);
+    },
+    parse: function (value, child, index) {
+        if (_.contains(value, child.id)) {
+            child.selected;
+        }
+        child.selected = _.contains(value, child.id) ? true : false;
+
+        if (_.isArray(child.children)) {
+            child.children = _.map(child.children, helper.parse.bind(this, value));
+        }
+        return child;
+    },
+    isAllSelected: function (data) {
+        if (!_.isArray(data.children) || !(data.children.length > 0)) {
+            return;
+        }
+        data.selected = _.reduce(data.children, function (result, child, index) {
+            return result && child.selected;
+        }, true);
+    }
+};
+
+module.exports = helper;
