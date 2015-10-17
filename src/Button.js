9a1e3feef1413e9667e83516b2342ff6eb8ec2b2
ChenXiao <chenxiao07@baidu.com>
2015-10-17 17:41:37 +0800

    给按钮加上ripple效果

--- src/Button.js
+++ src/Button.js
@@ -6,21 +6,49 @@
 var React = require('react');
 
 var Component = require('./Component');
+var TouchRipple = require('./ripples/TouchRipple');
 
 class Button extends Component {
 
+    getVariants(props) {
+        let variants = super.getVariants(props);
+        if (props.hasRipple) {
+            variants.push('ripple');
+        }
+        return variants;
+    }
+
     render() {
 
-        var props = this.props;
+        var {
+            props,
+            hasRipple,
+            label,
+            children,
+            ...other
+        } = this.props;
+
+        var content = label || children;
+
+        var style = hasRipple
+            ? {
+                position: 'relative'
+            }
+            : {};
 
         return (
-            <button {...props} className={this.getClassName()}>
-                {props.label || props.children}
+            <button {...other} className={this.getClassName()} style={style}>
+                {hasRipple ? <TouchRipple /> : null}
+                {content}
             </button>
         );
 
     }
 
 }
+
+Button.propTypes = {
+    hasRipple: React.PropTypes.bool
+};
 
 module.exports = Button;
