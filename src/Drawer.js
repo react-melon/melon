0c65570eefb4497cba0c0134881caf0a99157888
ChenXiao <chenxiao07@baidu.com>
2015-12-16 13:14:57 +0800

    给Drawer加上动画

--- src/Drawer.js
+++ src/Drawer.js
@@ -8,8 +8,16 @@ const React = require('react');
 const Mask = require('./Mask');
 
 const cx = require('./common/util/cxBuilder').create('Drawer');
+const domUtil = require('./common/util/dom');
 
 const {PropTypes} = React;
+
+const REVERT_POSITION = {
+    top: 'bottom',
+    bottom: 'top',
+    left: 'right',
+    right: 'left'
+};
 
 const Drawer = React.createClass({
 
@@ -38,23 +46,40 @@ const Drawer = React.createClass({
         const {props} = this;
         const {children, open, position, size, mask} = props;
 
+        let posValue = open ? 0 : -size;
+
+        if (size <= 0) {
+            const clientSize = position === 'bottom' || position === 'top'
+                    ? domUtil.getClientHeight()
+                    : domUtil.getClientWidth();
+            posValue = open ? -size : clientSize;
+        }
+
         const style = size > 0
             ? {
-                top: position === 'bottom' ? null : 0,
-                bottom: position === 'top' ? null : 0,
-                left: position === 'right' ? null : 0,
-                right: position === 'left' ? null : 0,
+                top: position === 'bottom' ? null : posValue,
+                bottom: position === 'top' ? null : posValue,
+                left: position === 'right' ? null : posValue,
+                right: position === 'left' ? null : posValue,
                 height: position === 'bottom' || position === 'top' ? size : null,
                 width: position === 'left' || position === 'right' ? size : null
             }
             : {
-                top: position === 'bottom' ? -size : 0,
-                bottom: position === 'top' ? -size : 0,
-                left: position === 'right' ? -size : 0,
-                right: position === 'left' ? -size : 0,
+                top: position === 'bottom' ? posValue : 0,
+                bottom: position === 'top' ? posValue : 0,
+                left: position === 'right' ? posValue : 0,
+                right: position === 'left' ? posValue : 0,
                 height: null,
                 width: null
             };
+
+        const visibilityTransitionDelay = open ? '0ms' : '400ms';
+        const transitionStyle = size > 0 ? position : REVERT_POSITION[position];
+
+        const transition = ''
+                +  transitionStyle + ' 400ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, '
+                +  'opacity 400ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, '
+                +  'visibility 0ms cubic-bezier(0.23, 1, 0.32, 1) ' + visibilityTransitionDelay;
 
         return (
             <div className={cx(props).build()}>
@@ -63,7 +88,11 @@ const Drawer = React.createClass({
                     style={{
                         ...style,
                         opacity: open ? 1 : 0,
-                        visibility: open ? 'visible' : 'hidden'
+                        visibility: open ? 'visible' : 'hidden',
+                        transition: transition,
+                        WebkitTransition: transition,
+                        MozTransition: transition,
+                        msTransition: transition
                     }}>
                     {children}
                 </div>
