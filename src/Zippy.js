0fd56729afd166898ae141003e3a3564e9886b94
chenxiao07 <chenxiao07@baidu.com>
2016-05-24 11:10:59 +0800

    修改Zippy的实现，改为用css来实现动画，如果设定了宽高才会出现动画

--- src/Zippy.js
+++ src/Zippy.js
@@ -4,47 +4,12 @@
  */
 
 import React, {PropTypes} from 'react';
-import ReactDOM from 'react-dom';
-import {Motion, spring} from 'react-motion';
-import {create} from './common/util/cxBuilder';
 
-import {getPosition} from './common/util/dom';
+import {create} from './common/util/cxBuilder';
 
 const cx = create('Zippy');
 
-function getStyle(horizontal, value) {
-
-    return {
-        [horizontal ? 'overflowX' : 'overflowY']: 'hidden',
-        [horizontal ? 'width' : 'height']: Math.round(value)
-    };
-
-}
-
 export default class Zippy extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            size: props.size,
-            first: true
-        };
-        this.main = null;
-    }
-
-    componentDidMount() {
-        const {isAdaptive, horizontal} = this.props;
-        if (isAdaptive && !this.state.size) {
-            this.setState({
-                size: getPosition(this.main)[horizontal ? 'width' : 'height'],
-                first: false
-            });
-        }
-    }
-
-    componentWillUnmount() {
-        this.main = null;
-    }
 
     render() {
 
@@ -53,55 +18,18 @@ export default class Zippy extends React.Component {
         const {
             expand,
             horizontal,
-            style,
-            isAdaptive,
             ...others
         } = props;
 
         /* eslint-disable fecs-min-vars-per-destructure */
 
-        const {
-            size = 0,
-            first
-        } = this.state;
-
-        const me = this;
-
-        let children = React.Children.only(props.children);
-        children = React.cloneElement(children, {
-            ref(main) {
-                me.main = ReactDOM.findDOMNode(main);
-            }
-        });
-
-        const className = cx(props).addStates({expand}).build();
-
-        // 刚开始没有动画
-        if (isAdaptive && !size && expand && first) {
-            return (
-                <div
-                    {...others}
-                    className={className}
-                    style={style}>
-                    {children}
-                </div>
-            );
-        }
+        const className = cx(props)
+            .addVariants(horizontal ? 'horizontal' : 'vertical')
+            .addStates({close: !expand})
+            .build();
 
         return (
-            <Motion style={{value: spring(expand ? size : 0)}}>
-                {({value}) =>
-                    <div
-                        {...others}
-                        className={className}
-                        style={{
-                            ...style,
-                            ...getStyle(horizontal, value)
-                        }}>
-                        {children}
-                    </div>
-                }
-            </Motion>
+            <div {...others} className={className} />
         );
 
     }
@@ -111,14 +39,11 @@ export default class Zippy extends React.Component {
 Zippy.displayName = 'Zippy';
 
 Zippy.propTypes = {
-    size: PropTypes.number,
     horizontal: PropTypes.bool,
-    expand: PropTypes.bool,
-    isAdaptive: PropTypes.bool
+    expand: PropTypes.bool
 };
 
 Zippy.defaultProps = {
     horizontal: false,
-    expand: false,
-    isAdaptive: false
+    expand: false
 };
