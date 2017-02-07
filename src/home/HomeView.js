e3c6646da8f3206e50388a8b7a7bd74fb878bb11
chenxiao07 <chenxiao07@baidu.com>
2017-02-07 19:47:32 +0800

    首页修改

--- src/home/HomeView.js
+++ src/home/HomeView.js
@@ -7,7 +7,6 @@ import React from 'react';
 import {connect} from 'ei';
 
 import Title from 'melon/Title';
-import Button from 'melon/Button';
 import Link from 'melon/Link';
 
 const cx = require('melon-core/classname/cxBuilder').create('Homeview');
@@ -22,30 +21,20 @@ class HomeView extends React.Component {
                     <div className={cx().part('header-logo').build()} />
                     <Title level={1}>melon</Title>
                     <p>A Set of React Components that Implement</p>
-                    <p>Google's Material Design</p>
-                </div>
-                <div className={cx().part('info').build()}>
-                    <p>
-                        Melon是一款基于
-                        <Link target="_blank" href="http://facebook.github.io/react/">React</Link>
-                        ，采用
-                        <Link target="_blank" href="https://www.google.com/design/spec/material-design/introduction.html">Google's Material Design</Link>
-                        设计的组件库。
-                    </p>
-                </div>
-                <div className={cx().part('install').build()}>
-                    <Title level={2}>安装</Title>
-                    <div>$ npm install melon --save</div>
+                    <p>{'Google\'s Material Design'}</p>
+                    <Link
+                        href="#/components?name=Button"
+                        variants={['raised', 'secondery', 'button']}>
+                        DEMO
+                    </Link>
                 </div>
                 <div className={cx().part('github').build()}>
-                    <p>请在github上关注我们的项目</p>
-                    <Button
-                        variants={['raised', 'primary']}
-                        onClick={() => {
-                            window.location = 'https://github.com/react-melon/melon/';
-                        }}>
+                    <p>Check out our repo for more information.</p>
+                    <Link
+                        href="https://github.com/react-melon/melon/"
+                        variants={['raised', 'primary', 'button']}>
                         GITHUB
-                    </Button>
+                    </Link>
                 </div>
                 <div className={cx().part('footer').build()}>
                     <p>©2015 Baidu</p>
