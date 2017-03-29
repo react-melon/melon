128a02961fd42bf6e7d7de6060db86f40d53da06
jinzhubaofu <leonlu@outlook.com>
2017-03-29 10:37:11 +0800

    使用 Layer 和 Popover 重构 Tooltip

--- test/components/Tooltip.spec.js
+++ test/components/Tooltip.spec.js
@@ -4,98 +4,113 @@
  */
 
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-
+import {mount} from 'enzyme';
 import then from '../then';
 
 import Tooltip from '../../src/Tooltip';
 import Button from '../../src/Button';
 
+import '../../src/css/theme/default/index.styl';
+import '../../src/css/base.styl';
+import '../../src/css/Tooltip.styl';
+
 describe('Tooltip', () => {
 
-    let component;
+    it('over mode', done => {
 
-    afterEach(() => {
-        component = null;
-    });
-
-    it('work', done => {
-
-        component = TestUtils.renderIntoDocument(
+        let wrapper = mount(
             <Tooltip content="这是一个 tooltip 呢">
                 <Button variants={['raised', 'primary']}>哟吼吼</Button>
             </Tooltip>
         );
 
-        expect(TestUtils.isCompositeComponent(component)).toBe(true);
-        expect(component.state.isShown).toBe(false);
+        expect(wrapper.state('open')).toBe(false);
 
-        const main = component.main;
-        expect(TestUtils.isDOMComponent(main)).toBe(true);
+        wrapper.find('.ui-tooltip').simulate('mouseenter');
 
-        TestUtils.Simulate.mouseEnter(main);
+        expect(wrapper.state('open')).toBe(true);
 
-        then(() => {
-            expect(component.state.isShown).toBe(true);
-            TestUtils.Simulate.mouseLeave(main);
-        })
-        .then(() => {
-            expect(component.state.isShown).toBe(false);
-            component.toggle();
-        })
-        .then(() => {
-            expect(component.state.isShown).toBe(true);
-            component.toggle();
-        })
-        .then(() => {
-            expect(component.state.isShown).toBe(false);
-            done();
-        });
+        wrapper.find('.ui-tooltip').simulate('mouseleave');
+
+        expect(wrapper.state('open')).toBe(false);
+
+        wrapper.unmount();
+
+        done();
+
     });
 
-    it('mode click', done => {
+    it('over click', done => {
 
-        component = TestUtils.renderIntoDocument(
-            <Tooltip mode="click" content="这是一个 tooltip 呢">
+        let wrapper = mount(
+            <Tooltip content="这是一个 tooltip 呢" mode="click">
                 <Button variants={['raised', 'primary']}>哟吼吼</Button>
             </Tooltip>
         );
 
-        expect(TestUtils.isCompositeComponent(component)).toBe(true);
-        expect(component.state.isShown).toBe(false);
+        expect(wrapper.state('open')).toBe(false);
 
-        const main = component.main;
+        wrapper.find('.ui-tooltip').simulate('click');
 
-        TestUtils.Simulate.click(main);
+        expect(wrapper.state('open')).toBe(true);
 
-        then(() => {
-            expect(component.state.isShown).toBe(true);
-            TestUtils.Simulate.click(main);
-        })
-        .then(() => {
-            expect(component.state.isShown).toBe(false);
+        setTimeout(() => {
+
+            document.body.click();
+
+            expect(wrapper.state('open')).toBe(false);
+
+            wrapper.unmount();
+
             done();
-        });
-    });
 
-    ['top', 'left', 'right', 'bottom'].forEach(function (pos) {
-
-        it('position ' + pos, () => {
-
-            component = TestUtils.renderIntoDocument(
-                <Tooltip direction={pos} content="这是一个 tooltip 呢" />
-            );
-
-            component.isShown = () => true;
-
-            const style = component.getPosition();
-
-            expect(style.opacity).toBe(1);
-            expect(style.left).not.toEqual(undefined);
-            expect(style.top).not.toEqual(undefined);
-
-        });
+        }, 20);
 
     });
+
+    // it('mode click', done => {
+    //
+    //     component = TestUtils.renderIntoDocument(
+    //         <Tooltip mode="click" content="这是一个 tooltip 呢">
+    //             <Button variants={['raised', 'primary']}>哟吼吼</Button>
+    //         </Tooltip>
+    //     );
+    //
+    //     expect(TestUtils.isCompositeComponent(component)).toBe(true);
+    //     expect(component.state.isShown).toBe(false);
+    //
+    //     const main = component.main;
+    //
+    //     TestUtils.Simulate.click(main);
+    //
+    //     then(() => {
+    //         expect(component.state.isShown).toBe(true);
+    //         TestUtils.Simulate.click(main);
+    //     })
+    //     .then(() => {
+    //         expect(component.state.isShown).toBe(false);
+    //         done();
+    //     });
+    // });
+    //
+    // ['top', 'left', 'right', 'bottom'].forEach(function (pos) {
+    //
+    //     it('position ' + pos, () => {
+    //
+    //         component = TestUtils.renderIntoDocument(
+    //             <Tooltip direction={pos} content="这是一个 tooltip 呢" />
+    //         );
+    //
+    //         component.isShown = () => true;
+    //
+    //         const style = component.getPosition();
+    //
+    //         expect(style.opacity).toBe(1);
+    //         expect(style.left).not.toEqual(undefined);
+    //         expect(style.top).not.toEqual(undefined);
+    //
+    //     });
+    //
+    // });
 
 });
