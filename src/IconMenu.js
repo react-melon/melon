c8e2f7e0ae5028854334a8c646bfe208b18f433a
jinzhubaofu <leonlu@outlook.com>
2017-04-05 15:11:42 +0800

    添加 Menu / IconMenu / DropMenu 三个组件

---
+++ src/IconMenu.js
@@ -0,0 +1,66 @@
+/**
+ * @file IconMenu
+ * @author leon <ludafa@outlook.com>
+ */
+
+import React, {Component, PropTypes} from 'react';
+import Button from './Button';
+import Icon from './Icon';
+import Menu from './Menu';
+import Popover from './Popover';
+
+export default class IconMenu extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.onClose = this.onClose.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+        this.state = {
+            open: false
+        };
+    }
+
+    onOpen() {
+        this.setState({
+            open: true
+        });
+    }
+
+    onClose() {
+        this.setState({
+            open: false
+        });
+    }
+
+    render() {
+
+        let {icon, children, style, maxHeight} = this.props;
+
+        return (
+            <div className="ui-icon-menu" ref="main" style={style}>
+                <Button onClick={this.onOpen}>
+                    <Icon icon={icon} />
+                </Button>
+                <Popover
+                    autoWidth={true}
+                    maxHeight={maxHeight}
+                    open={this.state.open}
+                    anchor={this.refs.main}
+                    onRequestClose={this.onClose}>
+                    <Menu onClose={this.onClose}>
+                        {children}
+                    </Menu>
+                </Popover>
+            </div>
+
+        );
+
+    }
+
+}
+
+IconMenu.propTypes = {
+    icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    children: PropTypes.arrayOf(PropTypes.element),
+    maxHeight: PropTypes.number
+};
